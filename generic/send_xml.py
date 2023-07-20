87272b68e94dcf1e9f8adeb89cea6f02f9d2426d
Utkarsh Choudhary <ufc5009@psu.edu>
2023-07-20 23:43:36 +0530

    Delete send_xml.py

--- generic/send_xml.py
+++
@@ -1,9 +0,0 @@
-#XML is eXtensive markup language
-import requests
-import xml.etree.ElementTree as ET
-
-api_url = "https://example.com/api/get_xml_data"
-
-response = requests.get(api_url)
-xml_data = ET.fromstring(response.text)
-print(xml_data)
